5001e234bec5ed65615dbf735aa0e81031cb2a0b
凌雪桑 <60049975+lingxues@users.noreply.github.com>
2022-07-10 12:42:23 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -49,8 +49,8 @@ module.exports = {
   // you provide. See the documentation for details.
   links: [
     {
-      name: 'GitHub',
-      link: 'https://github.com/spencerwooo/onedrive-vercel-index',
+      name: 'moeinu',
+      link: 'https://blog.moeinu.com',
     },
   ],
 
